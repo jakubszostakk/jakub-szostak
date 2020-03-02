38cb349701dcddd4e6fae9544fdfd05da263bdda
jakubszostakk <56103604+jakubszostakk@users.noreply.github.com>
2020-03-02 13:11:24 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle {
+public:
+    float radius_;
+};
+
+int main()
+{
+    Circle.radius_ = 1;
+    cout<<Circle.radius_<<endl;
+    return 0;
+}
